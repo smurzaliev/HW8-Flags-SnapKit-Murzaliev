8c35a5ae0bb7981e6e16f00e4928d3d5bf86d459
smurzaliev <samat.murzaliev@gmail.com>
2022-02-06 18:32:27 +0600

    Initial Commit

---
+++ HW8-Flags-SnapKit-Murzaliev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW8-Flags-SnapKit-Murzaliev
+//
+//  Created by Samat Murzaliev on 06.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
